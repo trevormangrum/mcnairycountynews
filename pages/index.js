1f8b9032a137d2320b840dd3ca0e2ceecb134e0a
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-03-22 00:40:02 -0500

    adds header and styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import Header from "components/Header";
 
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>McNairyCountyNews</title>
+        <title>McNairy County News</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1> Hello world </h1>
-
+        <Header/>
+        <h1>Hello</h1>
+        
     </div>
 
   )
