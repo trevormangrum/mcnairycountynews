32b19af172ff7ff902ad468045fdcd90950c2cda
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-04-20 12:09:53 -0500

    Fixes up contact form

---
+++ components/Loader/index.tsx
@@ -0,0 +1,21 @@
+import  React  from "react";
+
+const Loader: React.FC = () => {
+    return ( 
+        <div className="loader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+     );
+
+};
+
+export default Loader;
+
+
