d32e2657a7a258d9129c61432b44159eec1d9051
Trevor <mangrumtrevor@gmail.com>
2021-05-01 10:36:30 -0500

    Begins work on admin backend

--- pages/admin/[type]/[method]/index.tsx
+++ pages/admin/[type]/[method]/index.tsx
@@ -4,18 +4,29 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import SectionHeader from "components/SectionHeader";
 import InputGroup from "components/InputGroup";
+import { useQuery } from "@apollo/client";
+import { client } from "server/actions/Contentful";
+import queries from "server/actions/Contentful/queries";
 export default function AdminWorkPage() {
   const router = useRouter();
   const { type, method } = router.query;
   //Use GraphQL query to fetch from backend.
-  const data = type === "archives" ? "woof" : "meow";
-
+  const query = type == "articles" ? queries.articles.getTeasers : queries.archives.getArchivedPapers; 
+  const {data, loading, error} = useQuery(query, {
+    client: client
+  })
   return (
     <main className="admin-page">
       <Header />
       <div className="admin-wrapper">
         <SectionHeader text={`${method} ${method != "update" ? (method == "delete" ? "from" : "to") : ""} ${type}`} />
         <InputGroup inputType="search" inputName="search" inputPlaceholder={`Search for ${type}...`} labelText="Search" />
+        {data && !loading  && type == "articles" && (
+          <h1>Articles</h1>
+        )}
+        {data && !loading && type == "archives" &&  (
+          <h1>Archives</h1>
+        )}
 
       </div>
       <Footer />
