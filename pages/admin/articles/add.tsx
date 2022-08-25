c5629366c626e8c0a86eee502979a6c09082284e
Trevor Mangrum <mangrumtrevor@gmail.com>
2022-08-25 11:10:17 -0400

    fixes bug with date

--- pages/admin/articles/add.tsx
+++ pages/admin/articles/add.tsx
@@ -8,7 +8,7 @@ import SectionHeader from "components/SectionHeader";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import InputGroup from "components/InputGroup";
-import  Router, { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { NextPageContext } from "next";
 import urls from "utils/urls";
 
@@ -111,7 +111,7 @@ export default function CreateArticlePage() {
     });
     if (response.status === 200) {
       setLoading(false);
-     router.push(`${urls.baseUrl}${urls.pages.admin.index}`) 
+      router.push(`${urls.baseUrl}${urls.pages.admin.index}`);
     } else {
       setArticleValues({ ...articleValues, ["submissionError"]: true });
     }
@@ -128,7 +128,10 @@ export default function CreateArticlePage() {
 
         <form>
           <p>
-            Here you can add teasers to the website. Fill out the fields below and press the button to create a teaser. You will be redirected to the admin dashboard if creation was successful. Please do NOT add dashes (-) into the title.  
+            Here you can add teasers to the website. Fill out the fields below
+            and press the button to create a teaser. You will be redirected to
+            the admin dashboard if creation was successful. Please do NOT add
+            dashes (-) into the title.
           </p>
           <InputGroup
             inputType="text"
@@ -148,7 +151,7 @@ export default function CreateArticlePage() {
           />
           <InputGroup
             inputType="date"
-            inputName="posted"
+            inputName="date"
             inputPlaceholder="mm-dd-yyyy"
             labelText="Date Posted"
             value={articleValues.date}
@@ -204,20 +207,19 @@ export async function getServerSideProps(context: NextPageContext) {
   const resp = await fetch(`${urls.baseUrl}${urls.api.admin.validate}`, {
     headers: {
       cookie: cookie!,
-    }
-  })
+    },
+  });
   //If the cookie is not present, redirect to the login page.
-  if(resp.status === 401 && !context.req) {
+  if (resp.status === 401 && !context.req) {
     void Router.replace(`${urls.baseUrl}${urls.pages.login}`);
     return { props: {} };
   }
-  if(resp.status === 401 && context.req) {
+  if (resp.status === 401 && context.req) {
     context.res?.writeHead(302, {
       Location: `${urls.baseUrl}`,
     });
     context.res?.end();
     return { props: {} };
   }
-  return { props: {} }
+  return { props: {} };
 }
-
