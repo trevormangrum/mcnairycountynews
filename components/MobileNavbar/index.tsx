8cf150dda10351fa65c9ce2d82d86070b18a74ab
Trevor Mangrum <mangrumtrevor@gmail.com>
2022-10-26 15:13:32 -0400

    fixes bug with mobile menu items

--- components/MobileNavbar/index.tsx
+++ components/MobileNavbar/index.tsx
@@ -9,36 +9,42 @@ const MobileNavbar: React.FC<Props> = ({ open }) => {
       className="mobile-navbar"
       style={open ? { right: 0 } : { right: "-100%" }}
     >
-      <Link href="/">
-        <a href="/">Home</a>
-      </Link>
-      <Link href="/about">
-        <a href="">About Us</a>
-      </Link>
-      <Link href="/contact">
-        <a href="/contact">Contact Us</a>
-      </Link>
-      <Link href="/subscribe">
-        <a href="/subscribe">Subscribe</a>
-      </Link>
-      <Link href="/advertising">
-        <a href="/advertising">Advertising</a>
-      </Link>
-      <Link href="/e-paper">
-        <a href="/e-paper">Digital Edition Access</a>
-      </Link>
+      <div>
+        <Link href="/">
+          <a href="/">Home</a>
+        </Link>
+        <Link href="/about">
+          <a href="">About Us</a>
+        </Link>
+        <Link href="/contact">
+          <a href="/contact">Contact Us</a>
+        </Link>
+        <Link href="/subscribe">
+          <a href="/subscribe">Subscribe</a>
+        </Link>
+        <Link href="/advertising">
+          <a href="/advertising">Advertising</a>
+        </Link>
+        <Link href="/e-paper">
+          <a href="/e-paper">Digital Edition Access</a>
+        </Link>
 
-      <a target="_blank" rel="noopener" href="https://www.tnpublicnotice.com/">
-        Public Notices
-      </a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.tnpublicnotice.com/"
+        >
+          Public Notices
+        </a>
 
-      <a
-        target="_blank"
-        rel="noopener"
-        href="https://www.shackelfordfuneraldirectors.com/obituaries/"
-      >
-        Obituaries
-      </a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.shackelfordfuneraldirectors.com/obituaries/"
+        >
+          Obituaries
+        </a>
+      </div>
     </nav>
   );
 };
